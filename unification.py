ed039fa03b36533df115abc1e357ebce1255465e
Voloshin Bogdan <bvoloshin@sfedu.ru>
2021-10-28 17:23:26 +0300

    Implement unify_eq

--- unification.py
+++ unification.py
@@ -2,12 +2,21 @@ import data.context as ctx
 from defenitions import *
 import heapq
 
+var_num = 0
 
-def add_substituons(res, subs):
+
+def new_var(lower, upper):
+    global var_num
+    res = Variable('$Generated{0}'.format(var_num), lower, upper)
+    var_num += 1
+    return res
+
+
+def add_substitutions(res, subs):
     return res[0], res[1] + subs
 
 
-adds = Infix(add_substituons)
+adds = Infix(add_substitutions)
 
 
 def unify(constraints):
@@ -18,6 +27,8 @@ def unify(constraints):
         unify_eq(v)
     elif isinstance(v, Sub):
         unify_sub(constraints, v)
+    else:
+        raise Exception('fail')
 
 
 def unify_eq(constraints, c):
@@ -41,4 +52,27 @@ def unify_eq(constraints, c):
 
 
 def unify_sub(constraints, c):
-    pass
+    S, T = c.left, c.right
+    if S |vsub| T |out| 'vsub':
+        unify(constraints + ctx.outs['vsub'])
+    elif S |bel| Variable and T |bel| TypeVal and not S |infv| T and T |lay| S |out| 'lay':
+        X = new_var(T, S.upper)
+        return unify([S |rep| X] |at| constraints) |adds| [S |rep| X]
+    elif S |bel| TypeVal and T |bel| Variable and not T |infv| S and S |lay| T |out| 'lay':
+        X = new_var(T.lower, S)
+        return unify([T |rep| X] |at| constraints) |adds| [T |rep| X]
+    elif S |bel| Variable and T |bel| Variable and not S |infv| T and not T |infv| S and\
+            S.lower |gsub| T.upper |out| 'SgT':
+        Z = new_var(S.lower, T.upper)
+        X = Z |cros| T |out| 'ZT'
+        Y = Z |cros| S |out| 'ZS'
+        if X |vsub| Y |out| 'XvY':
+            return unify([S |rep| X, T |rep| Y] |at| (constraints + ctx.outs['SgT'] + ctx.outs['ZT'] + ctx.outs['ZS']
+                                                      + ctx.outs['XvY']))\
+                   |adds| [S |rep| X, T |rep| Y]
+        else:
+            return unify([S |rep| X, T |rep| Y] |at| (constraints + ctx.outs['SgT'] + ctx.outs['ZT'] + ctx.outs['ZS']
+                                                      + ctx.outs['XvY'] + [viewed(Sub(X, Y))]))\
+                   |adds| [S |rep| X, T |rep| Y]
+    else:
+        raise Exception('fail')
