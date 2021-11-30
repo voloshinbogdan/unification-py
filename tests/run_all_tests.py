3d7c3148de003cbccd168b338d1ca4a2ed0baef3
Voloshin Bogdan <bvoloshin@sfedu.ru>
2021-11-30 22:31:56 +0300

    Fix run all tests

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,12 +1,13 @@
-import tests.draw_graph_example as dge
-import tests.test_definitions as d
-import tests.test_unification as u
 import unittest
 
-
+import tests.draw_graph_example as dge
 dge.main()
+
+import tests.test_unification as u
 suite = unittest.TestLoader().loadTestsFromModule(u)
 unittest.TextTestRunner(verbosity=2).run(suite)
+
+import tests.test_definitions as d
 suite = unittest.TestLoader().loadTestsFromModule(d)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
